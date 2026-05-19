837b623cf6325e6c62e8864d8e3e4bbcfed1f804
Selma Yussuf <selmayussuf@macbook-air.home>
2026-05-19 19:09:51 +0100

    Build Dashboard/Medications pages & Navigation

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,27 @@
+from flask import render_template
+
 from application import app
+from application.models import Medication
 
 
 @app.route("/")
 def index():
-    return ""
+
+    medications = Medication.query.filter_by(
+        active=True
+        ).all()
+
+    return render_template(
+        "index.html",
+        medications=medications
+    )
+
+@app.route("/medications")
+def medications():
+
+    medications = Medication.query.all()
+
+    return render_template(
+        "medications.html",
+        medications=medications
+    )
